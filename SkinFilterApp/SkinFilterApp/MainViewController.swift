97348d6396e72ff081d1a1df602f5e3cd7637d60
Liudmila Dolontaeva <dolontaeva00@gmail.com>
2024-02-21 15:32:43 -0500

    filter at the whole image

--- SkinFilterApp/SkinFilterApp/MainViewController.swift
+++ SkinFilterApp/SkinFilterApp/MainViewController.swift
@@ -13,12 +13,7 @@ import YUCIHighPassSkinSmoothing
 class ViewController: UIViewController {
 
         
-    lazy var cameraButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Open Camera", for: .normal)
-        button.addTarget(self, action: #selector(openCamera), for: .touchUpInside)
-        return button
-    }()
+    private var cameraButton = UIButton()
 
     var previewLayer: AVCaptureVideoPreviewLayer!
     let captureSession = AVCaptureSession()
@@ -29,25 +24,39 @@ class ViewController: UIViewController {
     private let filter = YUCIHighPassSkinSmoothing()
     private let context = CIContext(options: [CIContextOption.workingColorSpace: CGColorSpaceCreateDeviceRGB()])
     
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLayout()
+        setUpStyle()
         setupCaptureSession()
     }
-
+    
     private func setupLayout() {
-           view.addSubview(cameraButton)
-           cameraButton.translatesAutoresizingMaskIntoConstraints = false
-           NSLayoutConstraint.activate([
-               cameraButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-               cameraButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-           ])
-           
-           view.addSubview(imageView)
+        view.addSubview(cameraButton)
+        view.addSubview(imageView)
+        
+        cameraButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cameraButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cameraButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func setUpStyle() {
         imageView.frame = view.frame
         imageView.contentMode = .scaleAspectFill
-       }
+        
+        cameraButton.titleLabel?.textColor = .blue
+        cameraButton.setTitleColor(.blue, for: .normal)
+        cameraButton.layer.cornerRadius = 15
+        cameraButton.setTitle("Open Camera", for: .normal)
+        cameraButton.addTarget(self, action: #selector(openCamera), for: .touchUpInside)
 
+    }
+    
     private func setupCaptureSession() {
         captureSession.beginConfiguration()
         let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
@@ -78,44 +87,38 @@ class ViewController: UIViewController {
         previewLayer.frame = view.bounds
         previewLayer.videoGravity = .resizeAspectFill
         view.layer.addSublayer(previewLayer)
-
+        
         DispatchQueue.global(qos: .userInitiated).async {
             self.captureSession.startRunning()
         }
-
-//        view.bringSubviewToFront(cameraButton)
         view.bringSubviewToFront(imageView)
     }
-
+    
 }
 
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
-    func captureOutput(
-        _ output: AVCaptureOutput,
-        didOutput sampleBuffer: CMSampleBuffer,
-        from connection: AVCaptureConnection
-    ) {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
             return
         }
         
-        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer).oriented(.leftMirrored)
         if let filteredCIImage = applySurfaceBlur(to: ciImage) {
-//            DispatchQueue.main.async {
-//                self.updatePreviewLayer(with: filteredCIImage)
-//            }
+            let outputCGImage = context.createCGImage(filteredCIImage, from: filteredCIImage.extent)
+            DispatchQueue.main.async { [weak self] in
+                self?.imageView.image = UIImage(cgImage: outputCGImage!)
+            }
         }
     }
-
+    
     func applySurfaceBlur(to ciImage: CIImage) -> CIImage? {
-        processImage(to: ciImage)
-        let filter = YUCIHighPassSkinSmoothing()
         filter.inputImage = ciImage
         filter.inputAmount = 0.9
         filter.inputRadius = 7.0 * ciImage.extent.width / 750.0 as NSNumber
         
         return filter.outputImage
     }
+    
     
     func processImage(to ciImage: CIImage) {
         filter.inputImage = ciImage
@@ -134,9 +137,8 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
             
             imageView.image = outputUIImage
         }
-        
     }
-
+    
     func updatePreviewLayer(with ciImage: CIImage) {
         let context = CIContext(options: nil)
         guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return }
@@ -145,5 +147,5 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
             self.imageView.image = uiImage
         }
     }
-
+    
 }
